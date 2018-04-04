edb18e4b54d6fe3188f52e8c405a69c10e35a4c7
Jiahong Wu <jiahongwu123@gmail.com>
2018-04-04 17:02:06 -0400

    configure db config file

--- config/config.php
+++ config/config.php
@@ -12,8 +12,8 @@ $baseConfig['appName'] = 'LightMVC Framework';
 
 $baseConfig['doctrine']['ORM']['em1'] = [
     'driver'   => 'pdo_mysql',
-    'host'     => '127.0.0.1',
-    'user'     => 'USERNAME',
-    'password' => 'PASSWORD',
-    'dbname'   => 'DBNAME',
+    'host'     => '127.0.0.1:9999',
+    'user'     => 'lightmvcuser',
+    'password' => '123',
+    'dbname'   => 'lightmvctestdb',
 ];
